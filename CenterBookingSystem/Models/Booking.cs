6198ff8baaeaca982cb7ddefd21134bf9203cd50
Iamneo <techadmin@iamneo.ai>
2023-09-28 10:16:04 +0000

    Backup Commit

--- CenterBookingSystem/Models/Booking.cs
+++ CenterBookingSystem/Models/Booking.cs
@@ -6,7 +6,9 @@ namespace CenterBookingSystem.Models
     public class Booking{
         public int BookingID{get;set;}
         public int SpaceID{get;set;}
-        
+        public int OrgaizerID{get;set;}
+        public DateTime EventDate{get;set;}
+        public TimeSpan TimeSlot{get;set;}
 
     }
     // write your Booking class here...
